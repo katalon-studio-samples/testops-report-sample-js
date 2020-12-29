a9d260d0b5a764b3e7710da25e53ddb2a4f992b9
lydoan1199 <ly.doan@katalon.com>
2020-12-29 10:57:55 +0700

    update

--- cypress-cli.js
+++ cypress-cli.js
@@ -1,12 +1,11 @@
 const cypress = require('cypress')
-const  MochaTesOpsReporter = require('@katalon/testops-mocha')
+const { CypressTestOpsReporter } = require('@katalon/testops-mocha');
 
-cypress.run({
-
-})
+  cypress.run({
+  })
   .then((results) => {
-    console.log('results: ', results);
-    // let reporter = new MochaTesOpsReporter();
+    const reporter = new CypressTestOpsReporter();
+    reporter.parseAndUploadTestResults(results);
   })
   .catch((err) => {
     console.error(err)
