968f0f4b5c2fca1bae36cb7b0cbd5105548fb595
lydoan1199 <ly.doan@katalon.com>
2020-12-29 12:03:01 +0700

    update

--- cypress-cli.js
+++ cypress-cli.js
@@ -4,7 +4,14 @@ const { CypressTestOpsReporter } = require('@katalon/testops-mocha');
   cypress.run({
   })
   .then((results) => {
-    const reporter = new CypressTestOpsReporter();
+    const config = {
+      username: "lydoan@kms-technology.com",
+      password: "Dtl#@1999",
+      basePath: "http://localhost:8444/",
+      projectId: 3,
+      reportFolder: "./testops-result"
+    }
+    const reporter = new CypressTestOpsReporter(config);
     reporter.parseAndUploadTestResults(results);
   })
   .catch((err) => {
--- cypress/integration/calculator2.test.ts
+++ cypress/integration/calculator2.test.ts
@@ -1,7 +1,7 @@
 import { add, subtract, multiply, divide } from '../helpers/calculator'
 const assert =  require('assert');
 
-describe('CalculatorTestCypress', function() {
+describe('CalculatorTestCypress2', function() {
     it('add', function () {
         assert.equal(add(345, 2), 347);
         assert.equal(add(2, 345), 347);
@@ -12,12 +12,15 @@ describe('CalculatorTestCypress', function() {
         assert.equal(subtract(200, 300), -100);
     });
 
+    //skip test case
     it('multiply', function () {
+        this.skip();
         assert.equal(multiply(367, 123), 45141);
         assert.equal(multiply(123, 367), 45141);
     });
 
+    //fail test case
     it('divide', function () {
-        assert.equal(divide(600, 2), 300);
+        assert.equal(divide(600, 2), 200);
     });
 });
